682290b8208ea9845c5a442b8b5ae398a1c231fb
Dayvidson Veiga <dayvidsonveiga@gmail.com>
2023-02-12 23:01:39 -0300

    First commit

---
+++ src/main/java/br/com/springdockerterraformaws/SpringDockerTerraformAwsApplication.java
@@ -0,0 +1,13 @@
+package br.com.springdockerterraformaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerTerraformAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerTerraformAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/springdockerterraformaws/SpringDockerTerraformAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springdockerterraformaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerTerraformAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
